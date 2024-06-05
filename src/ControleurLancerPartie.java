8ce39d094e46ba2d6948ca7d7053fd35bdde621e
ArKoSs145 <dore.benjamin10@gmail.com>
2024-06-05 16:20:42 +0200

    ControleurLancer, lettres et ajout de la fonction partie en cours dans Pendu

--- src/ControleurLancerPartie.java
+++ src/ControleurLancerPartie.java
@@ -2,7 +2,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.ButtonType;
 import java.util.Optional;
-
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent ;
+import javafx.scene.control.Button;
 /**
  * Contrôleur à activer lorsque l'on clique sur le bouton rejouer ou Lancer une partie
  */
@@ -18,10 +20,11 @@ public class ControleurLancerPartie implements EventHandler<ActionEvent> {
 
     /**
      * @param modelePendu modèle du jeu
-     * @param p vue du jeu
+     * @param vuePendu vue du jeu
      */
     public ControleurLancerPartie(MotMystere modelePendu, Pendu vuePendu) {
-        // A implémenter
+        this.modelePendu = modelePendu;
+        this.vuePendu = vuePendu;
     }
 
     /**
@@ -30,15 +33,30 @@ public class ControleurLancerPartie implements EventHandler<ActionEvent> {
      */
     @Override
     public void handle(ActionEvent actionEvent) {
-        // A implémenter
-    
-        Optional<ButtonType> reponse = this.vuePendu.popUpPartieEnCours().showAndWait(); // on lance la fenêtre popup et on attends la réponse
-        // si la réponse est oui
-        if (reponse.isPresent() && reponse.get().equals(ButtonType.YES)){
-            System.out.println("Ok !");
+        Button button = (Button) (actionEvent.getSource());
+        if (button.getText().equals("Nouveau mot")){
+            this.vuePendu.lancePartie();
+        } else{
+            if (this.vuePendu.getPartieEnCours()){
+                //la fenêtre popup est lancée et on attend la réponse
+                Optional<ButtonType> reponse = this.vuePendu.popUpPartieEnCours().showAndWait();
+                // si la réponse est positive
+                if (reponse.isPresent() && reponse.get().equals(ButtonType.YES)){
+                    System.out.println("Ok !");
+                    this.vuePendu.lancePartie();
+                } else{
+                    if (reponse.isPresent() && reponse.get().equals(ButtonType.NO)){
+
+                    } else {
+                        this.vuePendu.modeJeu();
+                    }
+                }
+
+            } else{
+                this.vuePendu.lancePartie();
+            }
         }
-        else{
-            System.out.println("D'ac !");
-        }
+
     }
+        
 }
